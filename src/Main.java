cfeee436eb107966404c6da32ec181bc1b228618
QA <test@neto.com>
2023-02-23 18:54:22 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int prise = 13676;
+        int mile = 20;
+
+        int bonus = prise / mile;
+
+        System.out.println("Количество бонусов: " + bonus);
+    }
+}
